bb313d5ba75f7f40c3c62285d84207d61ac4257f
mykola-telychko <75166107+mykola-telychko@users.noreply.github.com>
2023-08-10 21:48:49 +0300

    Initial commit

---
+++ index.ts
@@ -0,0 +1,17 @@
+console.clear();
+import { fromEvent, zip, pipe } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+// Example 3: get X/Y coordinates of drag start/finish (mouse down/up)
+// https://www.learnrxjs.io/learn-rxjs/operators/combination/zip
+
+const log = (event, val) => `${event}: ${JSON.stringify(val)}`;
+const getCoords = pipe(
+  map((e: MouseEvent) => ({ x: e.clientX, y: e.clientY }))
+);
+const documentEvent = (eventName) =>
+  fromEvent(document, eventName).pipe(getCoords);
+
+zip(documentEvent('mousedown'), documentEvent('mouseup')).subscribe((e) =>
+  console.log(`${log('start', e[0])} ${log('end', e[1])}`)
+);
